7ec3e2f710531aed6f075489f307de766a257399
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-01-03 12:27:54 -0500

    User Haris Javed saved changes to main.py at 01/03/2025, 17:27:53 UTC time.

--- 0a3276db-6193-4cce-9e45-07e04ab28015/main.py
+++ 0a3276db-6193-4cce-9e45-07e04ab28015/main.py
@@ -1,1 +1,41 @@
 #Type code here
+#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import SMA
+from surmount.logging import log
+import pandas as pd 
+import numpy as np 
+
+class TradingStrategy(Strategy):
+    @property
+    def assets(self):
+        # Define the assets to be used in the strategy
+        return ["BTC-USD"]
+
+    @property
+    def interval(self):
+        # The data interval desired for the strategy. Daily in this case.
+        return "5mins"
+    
+    def run(self, data):
+        #allocation_dict = {"BTCUSD": 1.0}
+
+        # Calculate the hitorical SMA's for BTCUSD
+        three_sma = SMA("BTC-USD", data["ohlcv"], length=3)
+        five_sma = SMA("BTC-USD", data["ohlcv"], length=5)
+        seven_sma = SMA("BTC-USD", data["ohlcv"], length=7)
+        ten_sma = SMA("BTC-USD", data["ohlcv"], length=10)
+
+        if three_sma[-1] > three_sma[-2]:
+            if three_sma[-1] > five_sma[-1]:
+                allocation_dict = {"BTC-USD": 1.0}
+            else:
+                allocation_dict = {"BTC-USD": 0.0}
+        else:
+            allocation_dict = {"BTC-USD": 0.0}
+        
+        if not allocation_dict:
+            allocation_dict = TargetAllocation({})
+
+        # Return the target allocation based on our logic
+        return TargetAllocation(allocation_dict)
